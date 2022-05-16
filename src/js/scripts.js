c50eef9d7d37c86305ac01f294472cc1a8d37b19
Arv <pandit.arv@gmail.com>
2022-05-16 04:57:49 -0600

    Added wireframe of Contact Page;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,17 +2,21 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const headerNav = document.querySelector("header nav ul");
 const headerNavLinks = document.querySelectorAll("header nav ul a");
 
-mobileMenu.addEventListener("click", () => {
+/**** Header Hamburger Menu ****/
+function toggleHamMenu() {
   mobileMenu.classList.toggle("active");
   headerNav.classList.toggle("active");
+}
+
+mobileMenu.addEventListener("click", () => {
+  toggleHamMenu();
 });
 
-headerNavLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    // if any link contains .active class
-    if (headerNav.classList.contains("active")) {
-      mobileMenu.classList.toggle("active");
-      headerNav.classList.toggle("active");
-    }
-  });
-});
+// headerNavLinks.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     // if any link contains .active class
+//     if (headerNav.classList.contains("active")) {
+//       toggleHamMenu();
+//     }
+//   });
+// });
