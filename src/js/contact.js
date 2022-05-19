df335c28807b0b8e2732aa704e5d7175d19f75d5
Arv <pandit.arv@gmail.com>
2022-05-19 13:10:58 -0600

    contact page validation for text area Subject input; changed the message in bottom of input  in contact html; changed the font size and weight of label, small;

--- src/js/contact.js
+++ src/js/contact.js
@@ -8,55 +8,23 @@ const commentInput = document.querySelector("#comment");
 const nameRegEx = /^[a-z\s]{1,25}$/i;
 const phoneRegEx = /^([2-9]\d{2})-(\d{3})-(\d{4})$/;
 const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const commentRegEx = /^[\w\d!@#\$%\^\&*\)\(+=._-]{1,500}$/gi;
 
 const inputVal = [fullName, telInput, emailInput, subjectInput, commentInput];
 
 formSubmit.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  //   if (!nameRegEx.test(fullName.value.trim())) {
-  //     fullName.classList.add("error");
-  //   } else {
-  //     fullName.classList.add("accept");
-  //   }
   validation(fullName, nameRegEx);
   validation(telInput, phoneRegEx);
-
   validation(emailInput, emailRegEx);
-  /** via Array Loop */
-  //   inputVal.forEach((item) => {
-  // let itemSibling = item.previousElementSibling.innerHTML;
-  // console.log(item.id, itemSibling);
-  //     item.value.trim() === "" ? errorMsgVis(item, `${item} is required`) : acceptMsgVis(item);
-  //   });
-
-  //   telephone number validation
-  //   let telVal = telInput.value.trim();
-
-  //   if (!phoneRegEx.test(telVal) && telVal === "") {
-  //     telInput.classList.add("error");
-  //   } else {
-  //     telInput.classList.add("accept");
-  //   }
-
-  //   // email validation
-  //   console.log(emailRegEx.test(emailInput.value));
-  //   if (!emailRegEx.test(emailInput.value) && emailInput.value === "") {
-  //     emailInput.classList.add("error");
-  //   } else {
-  //     emailInput.classList.add("accept");
-  //   }
+  validation(subjectInput, nameRegEx);
+  validation(commentInput, commentRegEx);
 });
 
 function validation(input, regEx) {
   const inputTrim = input.value.trim();
-  //   if (input.classList.contains("error") || input.classList.contains("accept")) {
-  //     input.classList.remove("error");
-  //     input.classList.remove("accept");
-  //   }
   if (regEx.test(input.value) === false || inputTrim === "") {
-    // console.log(typeof regEx.test(inputTrim));
-    // console.log(typeof input.value);
     input.classList.remove("accept");
     input.classList.add("error");
   } else {
@@ -64,18 +32,3 @@ function validation(input, regEx) {
     input.classList.add("accept");
   }
 }
-
-// function errorMsgVis(inputValue, message) {
-//   const smallTxt = document.querySelector("main small");
-//   smallTxt.innerText = message;
-//   const inputParent = inputValue.parentElement;
-//   //   inputParent.classList.remove("error");
-//   inputParent.classList.add("error");
-// }
-
-// function fieldName(input) {}
-
-// function acceptMsgVis(inputValue) {
-//   const inputParent = inputValue.parentElement;
-//   inputParent.classList.add("accept");
-// }
